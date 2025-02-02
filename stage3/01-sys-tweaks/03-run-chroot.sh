2c3135b3be630d97d15a88b53bf2b6cdb4bc06bb
frc4533-lincoln <132951735+frc4533-lincoln@users.noreply.github.com>
2025-02-02 15:12:22 -0500

    v9rv8uifvjsfvjs

---
+++ stage3/01-sys-tweaks/03-run-chroot.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+apt update
+apt install git
+
+wget -O rustup-init https://sh.rustup.rs
+chmod +x rustup-init
+./rustup-init -y --default-toolchain nightly --profile minimal
+. "$HOME/.cargo/env"
+
+git clone https://github.com/chalkydri/chalkydri.git
+
+pushd chalkydri
+cargo b -r
+mv target/release/chalkydri /usr/local/bin
+popd #chalkydri
+
+rm -r .rustup .cargo chalkydri
+
+systemctl enable chalkydri
