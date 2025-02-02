423caa0c59d7f0bc341d60cc7b047bb996966536
Lincoln <132951735+frc4533-lincoln@users.noreply.github.com>
2025-02-02 12:56:51 +0000

    Update 01-run.sh

--- stage3/00-install-packages/01-run.sh
+++ stage3/00-install-packages/01-run.sh
@@ -3,3 +3,20 @@
 on_chroot <<- EOF
 	apt-mark auto python3-pyqt5 python3-opengl
 EOF
+
+apt update
+apt install rustup
+rustup-init -y --default-toolchain nightly --profile minimal
+
+git clone https://github.com/chalkydri/chalkydri.git
+
+pushd chalkydri
+cargo b -r
+mv target/release/chalkydri "${ROOTFS_DIR}/usr/local/bin"
+popd #chalkydri
+
+cp files/chalkydri.service "${ROOTFS_DIR}/etc/systemd/system"
+
+on_chroot << EOF
+systemctl enable chalkydri
+EOF
