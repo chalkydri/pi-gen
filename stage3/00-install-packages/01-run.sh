08fa23763d72fe53f34be1dc8d5859cd0f80ba3d
Lincoln <132951735+frc4533-lincoln@users.noreply.github.com>
2025-02-02 13:58:42 +0000

    Update 01-run.sh

--- stage3/00-install-packages/01-run.sh
+++ stage3/00-install-packages/01-run.sh
@@ -4,19 +4,21 @@ on_chroot <<- EOF
 	apt-mark auto python3-pyqt5 python3-opengl
 EOF
 
-apt update
-apt install rustup
-rustup-init -y --default-toolchain nightly --profile minimal
+cp files/chalkydri.service "${ROOTFS_DIR}/etc/systemd/system"
+
+on_chroot << EOF
+wget -O rustup-init https://sh.rustup.rs
+chmod +x rustup-init
+./rustup-init -y --default-toolchain nightly --profile minimal
 
 git clone https://github.com/chalkydri/chalkydri.git
 
 pushd chalkydri
 cargo b -r
-mv target/release/chalkydri "${ROOTFS_DIR}/usr/local/bin"
+mv target/release/chalkydri /usr/local/bin
 popd #chalkydri
 
-cp files/chalkydri.service "${ROOTFS_DIR}/etc/systemd/system"
+rm -r .rustup .cargo chalkydri
 
-on_chroot << EOF
 systemctl enable chalkydri
 EOF
